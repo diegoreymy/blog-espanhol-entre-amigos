915388d1878cc60c4f3080991626ec910edb3a11
Diego Reymy <diegoreymy@mgmail.com>
2020-03-29 14:49:43 -0300

    ajustes visuales en la pagina de post

--- src/app/website/blog/components/post-detail/post-detail.component.ts
+++ src/app/website/blog/components/post-detail/post-detail.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { IPost } from '../../models/IPost.model';
 import { IPostDetail } from '../../models/iPostDetail.model';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.scss']
+  styleUrls: ['./post-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PostDetailComponent implements OnInit {
 
