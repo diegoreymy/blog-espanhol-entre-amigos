b9a34e19420041834529b53cb178881cdc8376df
Diego Reymy <diegoreymy@mgmail.com>
2020-04-12 20:51:49 -0300

    Mejorando imágenes de contacto

--- src/app/website/components/header/header.component.ts
+++ src/app/website/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faYoutube, faSpotify } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +13,7 @@ export class HeaderComponent implements OnInit {
   faFacebook = faFacebook;
   faInstagram = faInstagram;
   faYoutube = faYoutube;
+  faSpotify = faSpotify;
 
   constructor() { }
 
