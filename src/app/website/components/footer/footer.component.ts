36c9dbcba30482f679a25e3f7d8d0a7655cba0ae
Diego Reymy <diegoreymy@mgmail.com>
2020-04-19 15:23:42 -0300

    arreglando iconos del footer y eliminandos componentes innecesarios

--- src/app/website/components/footer/footer.component.ts
+++ src/app/website/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebook, faInstagram, faYoutube, faSpotify } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faYoutube = faYoutube;
+  faSpotify = faSpotify;
 
   constructor() { }
 
--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -16,16 +16,12 @@ const routes: Routes = [{
       loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule)
     },
     {
-      path: 'cursos',
-      loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
-    },
-    {
       path: 'blog',
       loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
     },
     {
       path: '**',
-      loadChildren: () => import('./not-found/not-found-module').then(m => m.NotFoundModule)
+      loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
     }
   ]
 }];
