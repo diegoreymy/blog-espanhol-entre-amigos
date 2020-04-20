c82e86ae4a898d805c4bfae5826bb78530e063d1
Diego Reymy <diegoreymy@mgmail.com>
2020-04-20 14:10:24 -0300

    colocando iframe opcional

--- src/app/website/home/components/about-espanhol-entre-amigos/about-espanhol-entre-amigos.component.ts
+++ src/app/website/home/components/about-espanhol-entre-amigos/about-espanhol-entre-amigos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutEspanholEntreAmigosComponent implements OnInit {
 
+  showVideo = false;
+
   constructor() { }
 
   ngOnInit() {
