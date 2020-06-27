0808685c0ecb47aab9f63d99361662048b565be2
Diego Reymy <diegoreymy@mgmail.com>
2020-06-27 14:02:00 -0300

    actualizando angular y colocando actualizaciones automaticas en el sw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, PLATFORM_ID, Inject } from '@angular/core';
+import { Component, PLATFORM_ID, Inject, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-
+import { SwUpdate } from '@angular/service-worker';
 import { filter } from 'rxjs/operators';
 import { Title } from '@angular/platform-browser';
 import { isPlatformBrowser } from '@angular/common';
@@ -12,27 +12,27 @@ declare var gtag;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(
     private router: Router,
     private titleService: Title,
-    // tslint:disable-next-line: ban-types
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {
+    private swUpdate: SwUpdate,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) { }
+
+  ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
-      const navEndEvents$ = this.router.events
-        .pipe(
-          filter(event => event instanceof NavigationEnd)
-        );
-      navEndEvents$.subscribe((event: NavigationEnd) => {
-        gtag('config', 'G-0G922NQS1C', {
-          page_path: event.urlAfterRedirects
-        });
-        this.titleService.setTitle(this.setTitle(event.urlAfterRedirects));
-        window.scrollTo(0, 0);
-      });
+      this.setAnalytics();
+      this.updatePWA();
     }
+  }
+
+  updatePWA() {
+    this.swUpdate.available.subscribe(value => {
+      console.log('update:', value);
+      window.location.reload();
+    });
   }
 
   setTitle(url: string): string {
@@ -40,5 +40,19 @@ export class AppComponent {
     const titleTrainCase = urlParts[urlParts.length - 1];
     const title = titleTrainCase.split('-').join(' ');
     return title === '' || title.includes('#') ? 'Español entre Amigos' : `Español entre Amigos - ${title}`;
+  }
+
+  setAnalytics() {
+    const navEndEvents$ = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      );
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-0G922NQS1C', {
+        page_path: event.urlAfterRedirects
+      });
+      this.titleService.setTitle(this.setTitle(event.urlAfterRedirects));
+      window.scrollTo(0, 0);
+    });
   }
 }
